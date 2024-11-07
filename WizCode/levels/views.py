4e939602c3f4c2a262c3e4bbd4117ef44931670d
Archisha-Dhyani <archishadhyani8@gmail.com>
2024-11-08 01:44:50 +0530

    Integreted game levels with bakend

--- WizCode/levels/views.py
+++ WizCode/levels/views.py
@@ -2,14 +2,30 @@
 from login.models import User
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-# Create your views here.
+from projects.models import FlowerData  
 
 def lvl_view_1(request):
-   context = {
-   }
-   return render(request, 'level1_Page.html', context)
+    # Initialize flower_info as an empty list by default
+    flower_info = []
+    
+    user_data = FlowerData.objects.filter(user=request.user).order_by('-created_at').first()
+    
+    if user_data and isinstance(user_data.data, list):
+        for flower in user_data.data:
+            if isinstance(flower, dict):
+                flower_info.append({
+                    'ID': flower.get('ID', 'N/A'),
+                    'Quality': flower.get('Quality', 'N/A'),
+                    'Size': f"{flower.get('Sepal.Length', 'N/A')} x {flower.get('Sepal.Width', 'N/A')}"
+                })
+    
+    # Store flower_info in the session
+    request.session['flower_info'] = flower_info
+    
+    return render(request, 'level1.html', {'flower_info': flower_info})
 
 def lvl_view_2(request):
-   context = {
-   }
-   return render(request, 'level2_Page.html', context)
+    # Retrieve flower_info from the session
+    flower_info = request.session.get('flower_info', [])
+    
+    return render(request, 'level2.html', {'flower_info': flower_info})
--- WizCode/projects/admin.py
+++ WizCode/projects/admin.py
@@ -1,1 +1,15 @@
 from django.contrib import admin
+from .models import FlowerData
+
+@admin.register(FlowerData)
+class FlowerDataAdmin(admin.ModelAdmin):
+    list_display = ('user', 'score', 'created_at')
+    list_filter = ('user', 'created_at')
+    search_fields = ('user__username',)
+    readonly_fields = ('created_at',)
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:  # editing an existing object
+            return self.readonly_fields + ('user',)
+        return self.readonly_fields
+    
